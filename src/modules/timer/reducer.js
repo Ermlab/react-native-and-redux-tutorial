9ebc22c9586cf9e53c4c42dcc158498cda4bad59
Grzegorz Długokęcki <grzesiek.dlugokecki@gmail.com>
2018-08-03 09:32:40 +0200

    Actions

--- src/modules/timer/reducer.js
+++ src/modules/timer/reducer.js
@@ -1,10 +1,31 @@
-const initialState = {};
+import * as actionTypes from './actionTypes'
+const initialState = {
+  isOn: false,
+  time: null,
+  boilType: null,
+};
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
+    case actionTypes.START_TIMER:
+      return {
+        ...state,
+        isOn: true,
+        time: action.time,
+      }
+    case actionTypes.STOP_TIMER:
+      return {
+        ...state,
+        isOn: false,
+      }
+    case actionTypes.SELECT_BOIL_TYPE:
+      return {
+        boilType: action.boilType
+      }
     default:
       return {
         ...state,
       };
   }
+
 }
